ca42f61580cf284a7b6ffa55ce87b9e073506df2
Sarat Chandra Prabhu Betha <saratbetha@in.ibm.com>
2021-04-29 17:11:34 +0530

    Working App, Initial Commit

---
+++ js/getId.js
@@ -0,0 +1,16 @@
+function initFingerprintJS() {
+    // Initialize an agent at application startup.
+    const fpPromise = FingerprintJS.load();
+
+    // Get the visitor identifier when you need it.
+    fpPromise
+    .then(fp => fp.get())
+    .then(result => {
+        // This is the visitor identifier:
+        const visitorId = result.visitorId
+        console.log(visitorId)
+        var fingerprint = document.getElementById("browserFingerprint");
+        fingerprint.innerHTML = visitorId;
+    })
+}
+// async src="//cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3/dist/fp.min.js" onload="initFingerprintJS()"
---
+++ js/location.js
@@ -0,0 +1,40 @@
+var longitude = document.getElementById("longitude");
+var latitude = document.getElementById("latitude");
+var locationError = document.getElementById("locationError");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        console.log("Geolocation is not supported by this browser.")
+        locationError.innerHTML = "Y";
+        longitude.innerHTML = "";
+        latitude.innerHTML = "";
+    }
+}
+
+function showPosition(position) {
+    longitude.innerHTML = position.coords.longitude;
+    latitude.innerHTML = position.coords.latitude;
+    locationError.innerHTML = "N";
+}
+
+function showError(error) {
+    locationError.innerHTML = "Y";
+    longitude.innerHTML = "";
+    latitude.innerHTML = "";
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            console.log("User denied the request for Geolocation.")
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.log("Location information is unavailable.")
+            break;
+        case error.TIMEOUT:
+            console.log("The request to get user location timed out.")
+            break;
+        case error.UNKNOWN_ERROR:
+            console.log("An unknown error occurred.")
+            break;
+    }
+}
