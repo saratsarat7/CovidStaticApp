2ce93de7a1ef54b4d08f16dd2b391c54b20da770
Sarat Chandra Prabhu Betha <saratbetha@in.ibm.com>
2021-05-03 19:38:36 +0530

    Back button handler.

--- js/pageHandler.js
+++ js/pageHandler.js
@@ -13,6 +13,25 @@ function routePage (pageValue) {
     var locationSelector = document.getElementById("locationSelector");
     var locationError = document.getElementById("locationError");
     var needType = document.getElementById("needType");
+    
+    var pageValueHTML = document.getElementById("pageValueHTML");
+    if (pageValue == "0") {
+        if (pageValueHTML.innerHTML == "list") {
+                window.location = '/';
+                return;
+            }
+        if (pageValueHTML.innerHTML == "category" &&
+            needType.innerHTML == "seeker") {
+                pageValue == "I need help";
+            }
+        if (pageValueHTML.innerHTML == "category" &&
+            needType.innerHTML == "helper") {
+                pageValue == "I can help";
+            }
+        if (pageValueHTML.innerHTML == "post") {
+                pageValue == "New Post";
+            }
+    }
 
     // Reset "new post" screen values when useris navigating again from home page.
     document.getElementById("name").value = "";
@@ -26,6 +45,9 @@ function routePage (pageValue) {
         selection.style.display = "none";
         userInput.style.display = "none";
         
+        // Set where you are right now for back button operation.
+        pageValueHTML.innerHTML = "list";
+
         // Set type of request, helper or seeker
         if (pageValue == "I need help") {
             needType.innerHTML = "seeker";
@@ -43,6 +65,10 @@ function routePage (pageValue) {
         listPage.style.display = "none";
         selection.style.display = "block";
         userInput.style.display = "none";
+
+        // Set where you are right now for back button operation.
+        pageValueHTML.innerHTML = "category";
+
         return;
     }
 
@@ -53,7 +79,10 @@ function routePage (pageValue) {
         listPage.style.display = "none";
         selection.style.display = "none";
         userInput.style.display = "block";
-        
+
+        // Set where you are right now for back button operation.
+        pageValueHTML.innerHTML = "post";
+
         // Show location button if location is not read already.
         if (locationError.innerHTML == "N") {
             locationSelector.style.display = "none";
